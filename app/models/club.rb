adc65dfbb490df6e8f7c1d68878881d6ceedbbfd
David Roberts <drob@drob.net>
2014-10-13 20:33:07 -0400

    Updated Data Model

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,4 +1,5 @@
 class Club < ActiveRecord::Base
   belongs_to :city
   belongs_to :technology
+  has_many :talks
 end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,10 @@
 class Feedback < ActiveRecord::Base
   belongs_to :talk
+  INVALID_COMMENTS = ['', 'NA', 'N/A', 'not applicable']
+
+  scope :filled_out,
+    -> { where.not(comment: INVALID_COMMENTS) }
+
+  scope :no_feedback,
+    -> { where(comment: INVALID_COMMENTS) }
 end
